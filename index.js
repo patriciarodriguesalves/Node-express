56a14852b5aae142dc81c64c0f4a3e21d5aed2a4
Patricia Rodrigues Alves <rpatricia@gmail.com>
2020-12-11 16:56:50 -0300

    API with Express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+userRoute(app)
+
+app.get('/', (req,res) => res.send('Hello word'));
+
+app.listen(port, () => console.log('Api rodando na porta 3000'));
+
+
